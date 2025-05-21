bef11bdb8d2dced1bde0e8a1c7875467f1617e7a
camdao <75600481+camdao@users.noreply.github.com>
2025-05-21 23:06:32 +0700

    feat: base time entity (#14)
    * chore: Jpa Auditing Config
    
    * feat: add BaseTimeEntity

---
+++ src/main/java/com/delivery/domain/model/BaseTimeEntity.java
@@ -0,0 +1,21 @@
+package com.delivery.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column @LastModifiedDate private LocalDateTime updatedAt;
+}
