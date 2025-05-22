09ecc0a2411e2c09e5a871a261eacbbc72146b18
camdao <luongcamdao@gmail.com>
2025-05-22 20:09:41 +0700

    feat: domain member

---
+++ src/main/java/com/delivery/domain/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.delivery.domain.member.domain;
+
+import com.delivery.domain.model.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String fullName;
+
+    private String profileImageUrl;
+
+    private String phone;
+
+    private String bio;
+}
