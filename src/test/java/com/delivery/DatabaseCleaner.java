12aab9c2e0c5a60b344888fa95ed27a19c9ad272
camdao <luongcamdao@gmail.com>
2025-05-23 17:55:18 +0700

    feat: add DatabaseCleaner

---
+++ src/test/java/com/delivery/DatabaseCleaner.java
@@ -0,0 +1,40 @@
+package com.delivery;
+
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.support.TransactionTemplate;
+
+public class DatabaseCleaner {
+    private DatabaseCleaner() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void clear(ApplicationContext applicationContext) {
+        var entityManager = applicationContext.getBean(EntityManager.class);
+        var jdbcTemplate = applicationContext.getBean(JdbcTemplate.class);
+        var transactionTemplate = applicationContext.getBean(TransactionTemplate.class);
+
+        transactionTemplate.execute(
+                status -> {
+                    entityManager.clear();
+                    deleteAll(jdbcTemplate, entityManager);
+                    return null;
+                });
+    }
+
+    private static void deleteAll(JdbcTemplate jdbcTemplate, EntityManager entityManager) {
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        for (String tableName : findDatabaseTableNames(jdbcTemplate)) {
+            entityManager
+                    .createNativeQuery("TRUNCATE TABLE %s".formatted(tableName))
+                    .executeUpdate();
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+
+    private static List<String> findDatabaseTableNames(JdbcTemplate jdbcTemplate) {
+        return jdbcTemplate.query("SHOW TABLES", (rs, rowNum) -> rs.getString(1)).stream().toList();
+    }
+}
