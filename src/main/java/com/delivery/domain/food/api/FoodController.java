3fbb419e6d41ae241d9070853eb847569723863e
camdao <luongcamdao@gmail.com>
2025-05-28 21:48:06 +0700

    feat: update,find food

--- src/main/java/com/delivery/domain/food/api/FoodController.java
+++ src/main/java/com/delivery/domain/food/api/FoodController.java
@@ -2,17 +2,18 @@ package com.delivery.domain.food.api;
 
 import com.delivery.domain.food.application.FoodService;
 import com.delivery.domain.food.dto.request.FoodCreateRequest;
+import com.delivery.domain.food.dto.request.FoodUpdateRequest;
 import com.delivery.domain.food.dto.response.FoodCreateResponse;
+import com.delivery.domain.food.dto.response.FoodFindAllResponse;
+import com.delivery.domain.food.dto.response.FoodUpdateResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "food", description = "Api food")
 @RestController
@@ -27,5 +28,18 @@ public class FoodController {
             @Valid @RequestBody FoodCreateRequest foodCreateRequest) {
         FoodCreateResponse foodCreateResponse = foodService.createFood(foodCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(foodCreateRequest);
+    }
+
+    @Operation(summary = "find all food", description = "return list food")
+    @GetMapping
+    public List<FoodFindAllResponse> foodFindAll() {
+        return foodService.findAllFood();
+    }
+
+    @Operation(summary = "update food", description = "update a food")
+    @PutMapping("/{foodId}")
+    public FoodUpdateResponse foodUpdate(
+            @Valid @RequestBody FoodUpdateRequest foodUpdateRequest, @PathVariable Long foodId) {
+        return foodService.updateFood(foodUpdateRequest, foodId);
     }
 }
