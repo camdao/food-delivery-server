4a3a9848f5e6ea003b1926cafe2f2a9b45ff19a8
camdao <luongcamdao@gmail.com>
2025-06-11 09:09:25 +0700

    feat: find Category

--- src/main/java/com/delivery/domain/category/application/CategoryService.java
+++ src/main/java/com/delivery/domain/category/application/CategoryService.java
@@ -3,8 +3,10 @@ package com.delivery.domain.category.application;
 import com.delivery.domain.category.domain.Category;
 import com.delivery.domain.category.dto.request.CategoryCreateRequest;
 import com.delivery.domain.category.dto.response.CategoryCreateResponse;
+import com.delivery.domain.category.dto.response.CategoryFindResponse;
 import com.delivery.domain.category.repository.CategoryRepository;
 import com.delivery.domain.food.dao.FoodRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +20,11 @@ public class CategoryService {
 
     public CategoryCreateResponse createCategory(CategoryCreateRequest createRequest) {
         Category category = categoryRepository.save(Category.createCategory(createRequest.name()));
-
         return CategoryCreateResponse.from(category);
+    }
+
+    public List<CategoryFindResponse> findCategory() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(CategoryFindResponse::of).toList();
     }
 }
