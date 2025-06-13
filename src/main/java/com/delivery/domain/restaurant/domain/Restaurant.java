43c764e5d8d59bd0a30dd19484ac8e733079318a
camdao <75600481+camdao@users.noreply.github.com>
2025-06-13 20:25:37 +0700

    feat: domain restaurant (#44)

---
+++ src/main/java/com/delivery/domain/restaurant/domain/Restaurant.java
@@ -0,0 +1,43 @@
+package com.delivery.domain.restaurant.domain;
+
+import com.delivery.domain.category.domain.Category;
+import jakarta.persistence.*;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_id")
+    private Long id;
+
+    private String name;
+
+    private String describe;
+
+    private String imgUrl;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "restaurant_category",
+            joinColumns = @JoinColumn(name = "restaurant_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories;
+
+    @Builder
+    public Restaurant(String name, String describe, String imgUrl) {
+        this.name = name;
+        this.describe = describe;
+        this.imgUrl = imgUrl;
+    }
+
+    public static Restaurant createRestaurant(String name, String describe) {
+        return Restaurant.builder().name(name).describe(describe).build();
+    }
+}
