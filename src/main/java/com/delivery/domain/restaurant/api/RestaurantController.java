42eba641848891988aa9ffe67d6ed061bfe13f68
camdao <75600481+camdao@users.noreply.github.com>
2025-06-16 22:36:17 +0700

    feat: api restaurant (#48)

---
+++ src/main/java/com/delivery/domain/restaurant/api/RestaurantController.java
@@ -0,0 +1,61 @@
+package com.delivery.domain.restaurant.api;
+
+import com.delivery.domain.restaurant.application.RestaurantService;
+import com.delivery.domain.restaurant.dto.request.RestaurantCreateRequest;
+import com.delivery.domain.restaurant.dto.request.RestaurantUpdateCategoryRequest;
+import com.delivery.domain.restaurant.dto.request.RestaurantUpdateRequest;
+import com.delivery.domain.restaurant.dto.response.RestaurantCreateResponse;
+import com.delivery.domain.restaurant.dto.response.RestaurantFindResponse;
+import com.delivery.domain.restaurant.dto.response.RestaurantUpdateCategoryResponse;
+import com.delivery.domain.restaurant.dto.response.RestaurantUpdateResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "restaurant", description = "api restaurant")
+@RequestMapping("restaurant")
+public class RestaurantController {
+    private final RestaurantService restaurantService;
+
+    @PostMapping("")
+    @Operation(summary = "create restaurant", description = "create a restaurant")
+    public RestaurantCreateResponse createRestaurant(
+            @Valid @RequestBody RestaurantCreateRequest restaurantCreateRequest) {
+        return restaurantService.createRestaurant(restaurantCreateRequest);
+    }
+
+    @Operation(summary = "find restaurant", description = "find a restaurant")
+    @GetMapping("/{restaurantId}")
+    public RestaurantFindResponse restaurantFind(@PathVariable Long restaurantId) {
+        return restaurantService.findRestaurant(restaurantId);
+    }
+
+    @Operation(summary = "update restaurant", description = "update a restaurant")
+    @PostMapping("/update/{restaurantId}")
+    public RestaurantUpdateResponse restaurantUpdate(
+            @PathVariable Long restaurantId,
+            @Valid @RequestBody RestaurantUpdateRequest restaurantUpdateRequest) {
+        return restaurantService.updateRestaurant(restaurantId, restaurantUpdateRequest);
+    }
+
+    @Operation(
+            summary = "update list category restaurant",
+            description = "update list category restaurant")
+    @PostMapping("/updateCategory/{restaurantId}")
+    public RestaurantUpdateCategoryResponse restaurantCategoryUpdate(
+            @PathVariable Long restaurantId,
+            @Valid @RequestBody RestaurantUpdateCategoryRequest restaurantUpdateCategoryRequest) {
+        return restaurantService.updateCategoryRestaurant(
+                restaurantId, restaurantUpdateCategoryRequest);
+    }
+
+    @Operation(summary = "delete restaurant", description = "delete restaurant")
+    @DeleteMapping("/{restaurantId}")
+    public void deleteRestaurant(@PathVariable Long restaurantId) {
+        restaurantService.deleteRestaurant(restaurantId);
+    }
+}
