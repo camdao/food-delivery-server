49581f3e0af829c3b05882fbfaf2676958712e41
camdao <luongcamdao@gmail.com>
2025-06-27 08:17:51 +0700

    test: find_a_category

--- src/test/java/com/delivery/domain/category/application/CategoryServiceTest.java
+++ src/test/java/com/delivery/domain/category/application/CategoryServiceTest.java
@@ -1,11 +1,15 @@
 package com.delivery.domain.category.application;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.delivery.domain.category.dto.request.CategoryCreateRequest;
 import com.delivery.domain.category.dto.response.CategoryCreateResponse;
+import com.delivery.domain.category.dto.response.CategoryFindResponse;
 import com.delivery.domain.category.repository.CategoryRepository;
 import com.delivery.domain.food.application.FoodService;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,5 +34,22 @@ public class CategoryServiceTest {
 
         // then
         assertEquals("name", categoryCreateResponse.name());
+    }
+
+    @Test
+    void find_a_category() {
+        // given
+        CategoryCreateRequest createRequest = new CategoryCreateRequest("name");
+        CategoryCreateResponse categoryCreateResponse =
+                categoryService.createCategory(createRequest);
+
+        // when
+        List<CategoryFindResponse> categoryList = categoryService.findCategory();
+
+        // then
+        assertThat(categoryList.size()).isEqualTo(1);
+        assertThat(categoryList)
+                .extracting("id", "name")
+                .containsExactlyInAnyOrder(tuple(categoryCreateResponse.id(), "name"));
     }
 }
